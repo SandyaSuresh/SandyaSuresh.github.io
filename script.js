88652d6ea3b29333b6556a42478dc48717023e1f
SandyaSuresh <sandya.suresh@gmail.com>
2025-09-26 20:32:53 -0400

    popups work!

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // NavBar and Footer Info
 // https://stackoverflow.com/a/70488082
 
+//Making popups
+// https://www.w3schools.com/howto/howto_js_popup.asp
+
 let navBar = `
 <nav>
     <ul class="navbar">
@@ -25,3 +28,34 @@ window.addEventListener('DOMContentLoaded', function (){
     document.getElementsByClassName("nav-bar")[0].innerHTML = navBar;
     document.getElementsByClassName("footer")[0].innerHTML = footer;
 });
+
+function makePopup(video, image, title, text){
+    let div = document.getElementById("popup");
+    div.innerHTML = `
+    <h4 class="popupcontent">` + title + `</h4>
+    <div class="popupcontent">
+        <video class="popupcontent" id="video" src="` + video + `" type="video/mp4"></video>
+        <p class="popupcontent">` + text +  `</p>
+        <button class="popupcontent" onclick="toggleshow()">close</button>
+    </div>
+    `;
+
+}
+
+function toggleshow(){
+    console.log("hi");
+    let popup = document.getElementById("popup");
+    if(popup.style.visibility == "visible"){
+        popup.style.visibility="hidden";
+    }else{
+        popup.style.visibility="visible";
+    }
+
+}
+
+function uselessAlarmClick(){
+    console.log("clickerd");
+    let desc = "adfad";
+    makePopup("videos/Useless-Alarm-Clock.mp4", "", "Useless Alarm Clock", desc);
+    toggleshow();
+}
